448595c5c628ae8e4dbf4def0dba555df6425ccd
ismailjosim <ismailjosim99@gmail.com>
2024-01-12 11:46:24 +0600

    Modify Readme file

---
+++ README.md
@@ -0,0 +1,11 @@
+# Topics
+
+## Day 01
+
+* Print Statement
+* String Manipulation
+* String Concatenation
+* Variables
+* Input Function
+
+🚀 Project: Band Name Generator
--- day-01/main.py
+++ day-01/main.py
@@ -1,4 +1,43 @@
-print("hello world!")
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+# print("hello world!")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# *** String Manipulation
+
+# print("Hello world!\nHello world!")
+
+# *** Add two string
+
+# print("Hello" + " " + "Ismail")
+
+
+# *** File Input
+
+# personName = input("What is your name?\n")
+# print("Hello! " + personName)
+
+# name1 = input("What is your name? ")
+# print(len(name1))
+
+# Switch Value from one variable to another
+# There are two variables, a and b from input
+# a = input()
+# b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+# c = a
+# a = b
+# b = c
+
+
+
+# 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+print("Welcome to the Band Generator")
+firstName = input("What's the name of the city you grew up in?\n")
+secondName = input("What's you pet name?\n")
+print("Your band name could be " + firstName + " " + secondName)
