670787d4f857f882edf371aa8cb29de03fbe7eb0
ismailjosim <ismailjosim99@gmail.com>
2024-01-12 12:09:29 +0600

    basic data types

---
+++ day-02/main.py
@@ -0,0 +1,18 @@
+# Data Types
+
+# String
+    # Subscription
+print("Hello"[2])
+print("Hello"[4])
+
+# Number (Int)
+print(123 + 456)
+
+# Number (float)
+
+print(3.14156)
+
+# Boolean
+True
+False
+
