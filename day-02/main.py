776db68bcfa50eb82367e63ab2f1b9855fff7888
ismailjosim <ismailjosim99@gmail.com>
2024-01-12 20:49:39 +0600

    calculation priority, BMI calculator

--- day-02/main.py
+++ day-02/main.py
@@ -29,8 +29,8 @@ print("Your name has " + newNumChar + " characters") # Output => Your name has 6
 # similar
 float(70 + float("100.5")) # => output: 170.5 convert string to number then add them
 
-# Coding Challenge: DATA TYPES
-# Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+# # Coding Challenge: DATA TYPES
+# # Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
 
 two_digit_number = input()
 # 🚨 Don't change the code above 👆
@@ -39,3 +39,41 @@ two_digit_number = input()
 b = int(two_digit_number[0])
 c = int(two_digit_number[1])
 print(b+c)
+
+
+# mathematical operators
+3 + 7
+5-3
+3*2
+6/3
+
+# 2**3 = 8 power
+
+# give priority PEMDAS => (),**, * /, + -
+
+print(3 * 3 + 3 / 3 - 3) # 6
+"""
+# ==> Steps
+        Multiply (3 * 3) = 9 Division (3/3) = 1.0
+        Add 9 + 1.0 = 10.0
+        Subtraction 10.0 - 3 = 7.0
+"""
+
+# Challenge change the above math order so that it can print 3.0
+print(3 * (3 + (3 / 3) - 3))
+# alternative
+print(3 * (3 + 3) / 3 - 3)
+
+
+height = input()
+weight = input()
+
+heightFloat = float(height)
+weightNum = int(weight)
+
+# My solution
+bmiCalculator = round(weightNum / (heightFloat * heightFloat))
+
+# Alternative Solution
+bmiCalculator = round(weightNum / heightFloat ** 2)
+print(bmiCalculator)
