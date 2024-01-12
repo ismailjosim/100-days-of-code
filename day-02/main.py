2c016db19bff0a6117f62fd550deeab0f5aa7612
ismailjosim <ismailjosim99@gmail.com>
2024-01-12 20:13:47 +0600

    coding challenge 1

--- day-02/main.py
+++ day-02/main.py
@@ -30,3 +30,12 @@ print("Your name has " + newNumChar + " characters") # Output => Your name has 6
 float(70 + float("100.5")) # => output: 170.5 convert string to number then add them
 
 # Coding Challenge: DATA TYPES
+# Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+b = int(two_digit_number[0])
+c = int(two_digit_number[1])
+print(b+c)
