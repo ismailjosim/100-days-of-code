a5ca91cab11addeefdd6f24fb914858bfc67eea1
ismailjosim <ismailjosim99@gmail.com>
2024-01-13 23:36:58 +0600

    nested elif statement

--- day-03/main.py
+++ day-03/main.py
@@ -1,27 +1,27 @@
-# # Conditional Statement (if-else statement)\
+# Conditional Statement (if-else statement)\
 
-# """ PYTHON IF-ELSE SYNTAX
-# if condition:
-#     do this
-# else:
-#     do this
-# """
+""" PYTHON IF-ELSE SYNTAX
+if condition:
+    do this
+else:
+    do this
+"""
 
-# height = int(input("Enter Your Heigh in CM: "))
+height = int(input("Enter Your Heigh in CM: "))
 
-# if height > 120:
-#     print("Here is your ticket!")
-# else:
-#     print("Sorry! your height is not enough to get the ticket!")
+if height > 120:
+    print("Here is your ticket!")
+else:
+    print("Sorry! your height is not enough to get the ticket!")
 
-# """ Comparison Operators
-#     1. > greater then
-#     2. < less then
-#     3. >= greater then or equal to
-#     4. <= less then or equal to
-#     5. == equal to
-#     6. != not equal to
-# """
+""" Comparison Operators
+    1. > greater then
+    2. < less then
+    3. >= greater then or equal to
+    4. <= less then or equal to
+    5. == equal to
+    6. != not equal to
+"""
 
 # Coding Challenge: 01
 number = int(input())
@@ -30,3 +30,29 @@ if number % 2 == 0:
     print("This is an even number.")
 else:
     print("This is an odd number.")
+
+# Nested if-else statement
+""" PYTHON IF-ELSE SYNTAX
+if condition:
+    if another condition:
+        do this
+    else:
+        do this
+else:
+    do this
+"""
+
+height = int(input("Enter Your Heigh in CM: "))
+
+
+if(height >120):
+    print("You can Ride the rollercoaster ride")
+    age = int(input("Enter Your age: "))
+    if age < 12:
+        print("Can pay $5 ticket")
+    elif age <= 18:
+        print("Can pay $7 ticket")
+    else:
+        print("Can pay $12 ticket")
+else:
+    print("can't ride")
