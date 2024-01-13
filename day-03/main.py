0726df6872aca6adfed77a23053464e0b5eb0c33
ismailjosim <ismailjosim99@gmail.com>
2024-01-14 00:50:19 +0600

    Coding challenge #3: BMICalculator 2.0

--- day-03/main.py
+++ day-03/main.py
@@ -58,33 +58,48 @@
 #     print("can't ride")
 
 
-# Coding Challenge #3: BMI calculation 2.0
-height = float(input())
-weight = int(input())
+# # Coding Challenge #3: BMI calculation 2.0
+# height = float(input())
+# weight = int(input())
 
-calcBMI = weight / height ** 2
+# calcBMI = weight / height ** 2
 
-# my solution
-if calcBMI > 18.5:
-    if (calcBMI > 18.5 and calcBMI < 25): # if calcBMI over 18 and below 25
-        print(f"Your BMI is {calcBMI}, you have a normal weight.")
-    elif(calcBMI >= 25 and calcBMI < 30): # if calcBMI Equal or over 25 and below 30
-        print(f"Your BMI is {calcBMI}, you are slightly overweight.")
-    elif(calcBMI >= 30 and calcBMI < 35): # if calcBMI Equal or over 30 and below 35
-        print(f"Your BMI is {calcBMI}, you are obese.")
+# # my solution
+# if calcBMI > 18.5:
+#     if (calcBMI > 18.5 and calcBMI < 25): # if calcBMI over 18 and below 25
+#         print(f"Your BMI is {calcBMI}, you have a normal weight.")
+#     elif(calcBMI >= 25 and calcBMI < 30): # if calcBMI Equal or over 25 and below 30
+#         print(f"Your BMI is {calcBMI}, you are slightly overweight.")
+#     elif(calcBMI >= 30 and calcBMI < 35): # if calcBMI Equal or over 30 and below 35
+#         print(f"Your BMI is {calcBMI}, you are obese.")
+#     else:
+#         print(f"Your BMI is {calcBMI}, You are clinically obese.")
+# else:
+#     print(f"Your BMI is {calcBMI}, You are underweight.")
+
+# # alternative solution
+# if calcBMI < 18.5:
+#      print(f"Your BMI is {calcBMI}, You are underweight.")
+# elif calcBMI < 25:
+#     print(f"Your BMI is {calcBMI}, you have a normal weight.")
+# elif calcBMI < 30:
+#     print(f"Your BMI is {calcBMI}, you are slightly overweight.")
+# elif calcBMI < 35:
+#     print(f"Your BMI is {calcBMI}, you are obese.")
+# else:
+#     print(f"Your BMI is {calcBMI}, You are clinically obese.")
+
+year = int(input("Enter Year "))
+
+# My solution
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year")
     else:
-        print(f"Your BMI is {calcBMI}, You are clinically obese.")
+        print("Leap year")
 else:
-    print(f"Your BMI is {calcBMI}, You are underweight.")
+    print("Not leap year")
 
-# alternative solution
-if calcBMI < 18.5:
-     print(f"Your BMI is {calcBMI}, You are underweight.")
-elif calcBMI < 25:
-    print(f"Your BMI is {calcBMI}, you have a normal weight.")
-elif calcBMI < 30:
-    print(f"Your BMI is {calcBMI}, you are slightly overweight.")
-elif calcBMI < 35:
-    print(f"Your BMI is {calcBMI}, you are obese.")
-else:
-    print(f"Your BMI is {calcBMI}, You are clinically obese.")
