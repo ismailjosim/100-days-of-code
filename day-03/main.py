64416c7da449d1c95cc75b2ef1033a311a0e1353
ismailjosim <ismailjosim99@gmail.com>
2024-01-13 23:19:22 +0600

    coding challenge-01: even/odd number

--- day-03/main.py
+++ day-03/main.py
@@ -1,24 +1,32 @@
-# Conditional Statement (if-else statement)\
+# # Conditional Statement (if-else statement)\
 
-""" PYTHON IF-ELSE SYNTAX
-if condition:
-    do this
+# """ PYTHON IF-ELSE SYNTAX
+# if condition:
+#     do this
+# else:
+#     do this
+# """
+
+# height = int(input("Enter Your Heigh in CM: "))
+
+# if height > 120:
+#     print("Here is your ticket!")
+# else:
+#     print("Sorry! your height is not enough to get the ticket!")
+
+# """ Comparison Operators
+#     1. > greater then
+#     2. < less then
+#     3. >= greater then or equal to
+#     4. <= less then or equal to
+#     5. == equal to
+#     6. != not equal to
+# """
+
+# Coding Challenge: 01
+number = int(input())
+
+if number % 2 == 0:
+    print("This is an even number.")
 else:
-    do this
-"""
-
-height = int(input("Enter Your Heigh in CM: "))
-
-if height > 120:
-    print("Here is your ticket!")
-else:
-    print("Sorry! your height is not enough to get the ticket!")
-
-""" Comparison Operators
-    1. > greater then
-    2. < less then
-    3. >= greater then or equal to
-    4. <= less then or equal to
-    5. == equal to
-    6. != not equal to
-"""
+    print("This is an odd number.")
