20721943537176ff50a0b02c2588733cf4c44218
ismailjosim <ismailjosim99@gmail.com>
2024-01-13 14:40:47 +0600

    conditional if-else statement, comparison operators

---
+++ day-03/main.py
@@ -0,0 +1,24 @@
+# Conditional Statement (if-else statement)\
+
+""" PYTHON IF-ELSE SYNTAX
+if condition:
+    do this
+else:
+    do this
+"""
+
+height = int(input("Enter Your Heigh in CM: "))
+
+if height > 120:
+    print("Here is your ticket!")
+else:
+    print("Sorry! your height is not enough to get the ticket!")
+
+""" Comparison Operators
+    1. > greater then
+    2. < less then
+    3. >= greater then or equal to
+    4. <= less then or equal to
+    5. == equal to
+    6. != not equal to
+"""
