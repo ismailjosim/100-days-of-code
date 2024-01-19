5e1a6a21e49db25d868a7a978570bbbf8e29b7ca
ismailjosim <ismailjosim99@gmail.com>
2024-01-19 22:13:13 +0600

    Coding Challenge #5: love complex calculator alternative solution

--- day-03/main.py
+++ day-03/main.py
@@ -180,3 +180,33 @@ elif loveSore > 40 and loveSore < 50:
      print(f"Your score is {loveSore}, you are alright together.")
 else:
     print(f"Your score is {loveSore}.")
+
+# alternative solution
+print("The Love Calculator is calculating your score...")
+name1 = input()  # What is your name?
+name2 = input()  # What is their name?
+combine_names = name1 + name2
+makeLowerName = combine_names.lower()
+
+t = makeLowerName.count("t")
+r = makeLowerName.count("r")
+u = makeLowerName.count("u")
+e = makeLowerName.count("e")
+
+first_digit = t + r + u + e
+
+l = makeLowerName.count("l")
+o = makeLowerName.count("o")
+v = makeLowerName.count("v")
+e = makeLowerName.count("e")
+
+second_digit = l + o + v + e
+
+score = int(str(first_digit) + str(second_digit))
+
+if score < 10 or score > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif score >= 40 and score <= 50:
+     print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
