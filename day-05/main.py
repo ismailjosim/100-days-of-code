ad96cdf7e8ec6b6a9c200b83044706823fe80377
ismailjosim <ismailjosim99@gmail.com>
2024-01-25 20:38:24 +0600

    Project: Create a Password Generator

--- day-05/main.py
+++ day-05/main.py
@@ -72,3 +72,57 @@ for number in range(1, 101):
     print("Buzz")
   else:
     print(number)
+
+# 🚀 Project: Password Generator
+import random
+letters = int(input("How Many Letters would you like in your password?\n"))
+symbols = int(input("How Many symbols would you like?\n"))
+numbers = int(input("How Many numbers would you like?\n"))
+
+# All English letters
+english_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+# All symbols
+allSymbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '{', '}', '|', '[', ']', '\\', ';', ':', "'", '"', ',', '.', '<', '>', '?', '/']
+
+# All numbers
+allNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+password = ""
+
+for char in range(1, letters + 1):
+  password += random.choice(english_letters)
+
+for symbol in range(1, symbols + 1):
+  password += random.choice(allSymbols)
+
+
+for symbol in range(1, numbers + 1):
+  password += random.choice(allNumbers)
+
+
+
+# Use sorted() to rearrange characters
+finalPass = ''.join(sorted(password))
+print(finalPass)
+
+# alternative solution
+result = []
+for char in range(1, letters + 1):
+  result.append(random.choice(english_letters))
+
+for symbol in range(1, symbols + 1):
+  result.append(random.choice(allSymbols))
+
+
+for symbol in range(1, numbers + 1):
+  result.append(random.choice(allNumbers))
+
+random.shuffle(result)
+
+password = ""
+for char in result:
+  password += char
+
+
+print(f"Your Generated password is: {password}")
