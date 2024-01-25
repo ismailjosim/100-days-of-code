b2f365602fe9c1c4803d47993f7567a29a2fd8ff
ismailjosim <ismailjosim99@gmail.com>
2024-01-25 19:17:35 +0600

    Coding challenge 01: average height

--- day-05/main.py
+++ day-05/main.py
@@ -1,4 +1,20 @@
-phoneBrand = ["apple", "SamSang","Nokia","Xiaomi"]
+# phoneBrand = ["apple", "SamSang","Nokia","Xiaomi"]
 
-for brand in phoneBrand:
-    print(brand)
+# for brand in phoneBrand:
+#     print(brand)
+
+# COding Challenge: 01
+# Input a Python list of student heights
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+# Write your code below this row 👇
+height = 0
+for student in student_heights:
+  height += student
+  avg = height / len(student_heights)
+  print(f"total height = {height}")
+  print(f"number of students = {len(student_heights)}")
+  print(f"average height = {avg}")
